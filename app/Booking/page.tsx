e9d9ba2f483cee6ca554c8d3d25fcad2b631f634
Alfredo Venturina <alfredoventurina@gmail.com>
2026-01-06 11:33:36 +0800

    latest stable

--- app/Booking/page.tsx
+++ app/Booking/page.tsx
@@ -1,10 +1,9 @@
-import {getBookingsAction} from '@/app/actions'
+import {getAllBookingsAction} from '@/app/actions'
 import BookingClient from './BookingClient';
-
 
 export default async function BookingPage(){
   //fetch existing bookings from DB before loading UI
-  const initialBookings = await getBookingsAction();
+  const initialBookings = await getAllBookingsAction();  
 
   return <BookingClient initialBookings={initialBookings}/>;
 }
