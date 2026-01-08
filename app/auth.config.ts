dcdf345541b5cbd1fa48611818901ce18d84796b
unknown <alfredo.venturina@GTPH-LPC095.cscc.local>
2026-01-09 02:25:21 +0800

    added comments

--- app/auth.config.ts
+++ app/auth.config.ts
@@ -1,23 +1,40 @@
-// app/auth.config.ts
+//import type definition from nextauth to ensure config object matches it
+
 import type { NextAuthConfig } from "next-auth";
 
+//intialize and export authConfig object
+//contains authentication rules that will be imported into middleware.ts and auth.ts
 export const authConfig = {
   pages: {
-    signIn: "/login",
+    signIn: "/login", //if something goes wrong or user needs to sign in, they are sent to /app/login/page.tsx
   },
+
+  //callback functions that run during specific authentication events
   callbacks: {
+    //authorized function runs via Middleware on every request to the site
+    //it decides if the user is allowed to see the page they requested
     authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
+      //!! boolean conversion
+      const isLoggedIn = !!auth?.user; //checks if auth.user exists
+
+      //identify which pages are private/protected
+      //this will return true if a user is visiting a private page
       const isProtectedRoute = 
         nextUrl.pathname.startsWith("/Booking") || 
         nextUrl.pathname.startsWith("/MyBookings");
 
+      //check if user is accessing a protected route
       if (isProtectedRoute) {
-        if (isLoggedIn) return true;
+        if (isLoggedIn) return true; //if logged in, let user pass
         return false; // Redirects to /login
       }
+
+      //this is for public routes
       return true;
     },
   },
-  providers: [], // Keep this empty here
+  // list of all authentication strategies (like oauth), keep empty for standard
+  providers: [],
+
+  //type validation using the type we imported
 } satisfies NextAuthConfig;
